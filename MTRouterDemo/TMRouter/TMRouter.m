de8ad3228bc729db5b1d6ab2b3b025a1c49de215
dongxuwei <287178790@qq.com>
2018-02-26 10:33:14 +0800

    注释完善一下

--- MTRouterDemo/TMRouter/TMRouter.m
+++ MTRouterDemo/TMRouter/TMRouter.m
@@ -62,7 +62,7 @@ _Pragma("clang diagnostic pop") \
 }
 
 /**
- 此方法用来初始化参数（控制器初始化方法默认为 initViewControllerParam）
+ 此方法用来初始化参数（控制器初始化方法默认为 initViewControllerParam。初始化方法你可以自定义，前提是VC必须实现它。要想灵活一点，也可以加一个参数actionName,当做参数传入。不过这样你就需要修改此方法了)。
  @param controller 获取到的实例VC
  @param paramdic 实例化参数
  @param vcName 控制器名字
@@ -75,8 +75,8 @@ _Pragma("clang diagnostic pop") \
         NSLog(@"目标类:%@未定义:%@方法",controller,@"initViewControllerParam:");
         return controller;
     }
+    //在初始化参数里面添加一个key信息，方便控制器中查验路由信息
     if(paramdic == nil){
-        //如果参数为空 URLKEY 页面唯一路径标识别
         paramdic = [[NSMutableDictionary alloc] init];
         
         [paramdic setValue:vcName forKey:@"URLKEY"];
